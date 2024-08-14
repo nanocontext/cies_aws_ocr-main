3cb1658a3769c61e4fc7da70c3a942b88b10bff1
Chris Beckey <cbeckey@gmail.com>
2024-08-14 08:27:35 -0400

    initial work to get pytest running

--- tests/unit/test_cies_ocr_core.py
+++ tests/unit/test_cies_ocr_core.py
@@ -2,8 +2,13 @@ import json
 import os
 import mimetypes
 from src.cies_ocr_core import *
-from aws_lambda_powertools import Logger, Metrics, Tracer
 
+cies_ocr_core = CiesOcrCore(
+    os.getenv('SOURCE_BUCKET'), 
+    os.getenv('DESTINATION_BUCKET'), 
+    os.getenv('TEXTRACT_SERVICE_ROLE'), 
+    os.getenv('TEXTRACT_STATUS_TOPIC'), 
+    os.getenv("AWS_REGION"))
 
 def test_answer():
     updated_tags = cies_ocr_core.update_tag_set({"a": 1}, {"b": 2})
