c6385ce43c49aac7afbae4531fd063fb465a97c7
Chris Beckey <cbeckey@gmail.com>
2024-08-16 13:40:48 -0400

    enable pytest

--- tests/unit/test_cies_ocr_core.py
+++ tests/unit/test_cies_ocr_core.py
@@ -14,4 +14,4 @@ def test_answer():
     # updated_tags = cies_ocr_core.update_tag_set({"a": 1}, {"b": 2})
     # assert "a" in updated_tags
     # assert "b" in updated_tags
-    assert True
+    assert False
